ebcc2fe7609c1261033605f3d88006bb382589ca
Koala Yeung <koalay@gmail.com>
2015-01-24 13:47:19 +0800

    Implemented cache storing
    Really store response in database.

--- cache.go
+++ cache.go
@@ -19,6 +19,29 @@ type SqlCache struct {
 }
 
 func (c *SqlCache) Add(url string, ctx Context, r *Response) (err error) {
-	// TODO: store the response into database
+	stmt, err := c.DB.Prepare("INSERT INTO `cachedfetch_cache` " +
+		"(`url`, `context`, `context_time`, `fetched`, " +
+		"`status`, `status_code`, `proto`, `content_length`, " +
+		"`transfer_encoding`, `header`, `trailer`, `request`, `body`)" +
+		"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		return
+	}
+
+	_, err = stmt.Exec(
+		r.URL,
+		r.Context,
+		r.ContextTime.Unix(),
+		r.Fetched.Unix(),
+		r.Status,
+		r.StatusCode,
+		r.Proto,
+		r.ContentLength,
+		r.TransferEncodingJson,
+		r.HeaderJson,
+		r.TrailerJson,
+		r.RequestJson,
+		r.Body)
+
 	return
 }
